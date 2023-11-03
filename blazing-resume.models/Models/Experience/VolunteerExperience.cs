91519f9f765c6ee3ddf11c62988c888d80a5a90b
S.Spee <stef.spee@innvolve.nl>
2023-11-03 11:11:22 +0100

    Setup some mock-data using Bogus. Handy to use while using QuestPDF.Previewer project to create a pdf-template.

--- blazing-resume.models/Models/Experience/VolunteerExperience.cs
+++ blazing-resume.models/Models/Experience/VolunteerExperience.cs
@@ -5,8 +5,8 @@ public sealed class VolunteerExperience
     public string Organization { get; set; }
     public string Position { get; set; }
     public string Url { get; set; }
-    public string StartDate { get; set; }
-    public string EndDate { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
     public string Summary { get; set; }
     public List<string> Highlights { get; set; }
 }
