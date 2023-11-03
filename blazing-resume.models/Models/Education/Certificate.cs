dc9d00e253be745fdf6905965120b93f39e84e04
S.Spee <stef.spee@innvolve.nl>
2023-11-03 11:11:22 +0100

    Setup some mock-data using Bogus. Handy to use while using QuestPDF.Previewer project to create a pdf-template.

--- blazing-resume.models/Models/Education/Certificate.cs
+++ blazing-resume.models/Models/Education/Certificate.cs
@@ -3,7 +3,7 @@ namespace blazing_resume.models;
 public sealed class Certificate
 {
     public string Name { get; set; }
-    public string Date { get; set; }
+    public DateTime Date { get; set; }
     public string Issuer { get; set; }
     public string Url { get; set; }
 }
--- blazing-resume.models/Models/Experience/ProjectExperience.cs
+++ blazing-resume.models/Models/Experience/ProjectExperience.cs
@@ -3,8 +3,8 @@ namespace blazing_resume.models;
 public sealed class ProjectExperience
 {
     public string Name { get; set; }
-    public string StartDate { get; set; }
-    public string EndDate { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
     public string Description { get; set; }
     public List<string> Highlights { get; set; }
     public string Url { get; set; }
