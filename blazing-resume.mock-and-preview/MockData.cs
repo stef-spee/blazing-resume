e4c059e4b15723c4e216ec777178772bda00490f
S.Spee <70277761+stef-spee@users.noreply.github.com>
2023-11-07 15:10:03 +0100

    Add mock data (#3)
    * Initial project setup
    
    * Setup some mock-data using Bogus. Handy to use while using QuestPDF.Previewer project to create a pdf-template.
    
    * Initial project setup
    
    * Setup some mock-data using Bogus. Handy to use while using QuestPDF.Previewer project to create a pdf-template.
    
    * added bogus data
    solved merge-conflicts
    re-added pdf project
    
    ---------
    
    Co-authored-by: S.Spee <stef.spee@innvolve.nl>
    Co-authored-by: Stef Spee <s.spee@arcady.nl>

--- blazing-resume.mock-and-preview/MockData.cs
+++ blazing-resume.mock-and-preview/MockData.cs
@@ -1,7 +1,5 @@
 ﻿using blazing_resume.models;
 using Bogus;
-using System;
-using System.Net;
 
 namespace blazing_resume.mock;
 
@@ -11,7 +9,17 @@ public static class MockData
         new Faker<ResumeModel>()
             .CustomInstantiator(_ => new ResumeModel())
             .RuleFor(o => o.Basics, _ => GetUserInfo())
-            .RuleFor(o => o.Work, _ => GetWorkExperience());
+            .RuleFor(o => o.Work, _ => GetWorkExperience())
+            .RuleFor(o => o.Volunteer, _ => GetVolunteerExperiences())
+            .RuleFor(o => o.Education, _ => GetEducations())
+            .RuleFor(o => o.Award, _ => GetAwards())
+            .RuleFor(o => o.Certificates, _ => GetCertificates())
+            .RuleFor(o => o.Publications, GetPublications())
+            .RuleFor(o => o.Skills, GetSkills())
+            .RuleFor(o => o.Languages, GetLanguageProficiencies())
+            .RuleFor(o => o.Interests, GetInterests())
+            .RuleFor(o => o.References, GetReferences())
+            .RuleFor(o => o.Projects, GetProjects());
 
     private static Basics GetUserInfo()
     {
@@ -56,4 +64,97 @@ public static class MockData
                 .RuleFor(o => o.Summary, f => f.Lorem.Paragraph())
                 .RuleFor(o => o.Highlights, f => new List<string>() { f.Lorem.Sentence() })
                 .GenerateBetween(2, 4);
+
+    private static List<VolunteerExperience> GetVolunteerExperiences() =>
+            new Faker<VolunteerExperience>()
+                .CustomInstantiator(_ => new VolunteerExperience())
+                .RuleFor(o => o.Organization, f => f.Company.CompanyName())
+                .RuleFor(o => o.Position, f => f.Lorem.Word())
+                .RuleFor(o => o.Url, f => f.Internet.Url())
+                .RuleFor(o => o.StartDate, f => f.Date.Past())
+                .RuleFor(o => o.EndDate, f => f.Date.Past())
+                .RuleFor(o => o.Summary, f => f.Lorem.Paragraph())
+                .RuleFor(o => o.Highlights, f => new List<string>() { f.Lorem.Sentence() })
+                .GenerateBetween(1, 2);
+
+    private static List<Education> GetEducations() =>
+            new Faker<Education>()
+            .CustomInstantiator(_ => new Education())
+            .RuleFor(o => o.Institution, f => f.Company.CompanyName())
+            .RuleFor(o => o.Url, f => f.Internet.Url())
+            .RuleFor(o => o.Area, f => f.Lorem.Words(2).ToString())
+            .RuleFor(o => o.StudyType, f => f.Lorem.Word())
+            .RuleFor(o => o.StartDate, f => f.Date.Past())
+            .RuleFor(o => o.EndDate, f => f.Date.Past())
+            .RuleFor(o => o.Score, f => f.Random.Decimal(6.0m, 10.0m).ToString())
+            .RuleFor(o => o.Courses, f => new List<string> { f.Lorem.Sentence() })
+            .GenerateBetween(1, 2);
+
+    private static List<Award> GetAwards() =>
+            new Faker<Award>()
+            .CustomInstantiator(_ => new Award())
+            .RuleFor(o => o.Title, f => f.Lorem.Word())
+            .RuleFor(o => o.Date, f => f.Date.Past())
+            .RuleFor(o => o.Awarder, f => f.Company.CompanyName())
+            .RuleFor(o => o.Summary, f => f.Lorem.Paragraph())
+            .GenerateBetween(1, 2);
+
+    private static List<Certificate> GetCertificates() =>
+            new Faker<Certificate>()
+            .CustomInstantiator(_ => new Certificate())
+            .RuleFor(o => o.Name, f => f.Lorem.Word())
+            .RuleFor(o => o.Date, f => f.Date.Past())
+            .RuleFor(o => o.Issuer, f => f.Company.CompanyName())
+            .RuleFor(o => o.Url, f => f.Internet.Url())
+            .GenerateBetween(1, 2);
+
+    private static List<Publication> GetPublications() =>
+            new Faker<Publication>()
+            .CustomInstantiator(_ => new Publication())
+            .RuleFor(o => o.Name, f => f.Lorem.Word())
+            .RuleFor(o => o.ReleaseDate, f => f.Date.Past())
+            .RuleFor(o => o.Publisher, f => f.Company.CompanyName())
+            .RuleFor(o => o.Url, f => f.Internet.Url())
+            .RuleFor(o => o.Summary, f => f.Lorem.Sentence())
+            .GenerateBetween(1, 2);
+
+    private static List<Skill> GetSkills() =>
+            new Faker<Skill>()
+            .CustomInstantiator(_ => new Skill())
+            .RuleFor(o => o.Name, f => f.Lorem.Word())
+            .RuleFor(o => o.Level, f => f.Lorem.Word())
+            .RuleFor(o => o.Keywords, f => f.Lorem.Words(5).ToList())
+            .GenerateBetween(1, 2);
+
+    private static List<LanguageProficiency> GetLanguageProficiencies() =>
+            new Faker<LanguageProficiency>()
+            .CustomInstantiator(_ => new LanguageProficiency())
+            .RuleFor(o => o.Language, f => f.PickRandom("English", "Dutch", "German", "Spanish", "French"))
+            .RuleFor(o => o.Fluency, f => f.PickRandom("Native", "Proficient", "Good", "Basic"))
+            .GenerateBetween(1, 2);
+
+    private static List<Interest> GetInterests() =>
+            new Faker<Interest>()
+            .CustomInstantiator(_ => new Interest())
+            .RuleFor(o => o.Name, f => f.PickRandom("Wildlife", "Cars", "Gaming", "Software", "Photography", "Baking", "Cooking", "Arts and Crafts"))
+            .RuleFor(o => o.Keywords, f => f.Lorem.Words(5).ToList())
+            .GenerateBetween(1, 2);
+
+    private static List<ReferenceContact> GetReferences() =>
+            new Faker<ReferenceContact>()
+            .CustomInstantiator(_ => new ReferenceContact())
+            .RuleFor(o => o.Name, f => f.Person.FullName)
+            .RuleFor(o => o.Reference, _ => "Reference...")
+            .GenerateBetween(1, 2);
+
+    private static List<ProjectExperience> GetProjects() =>
+            new Faker<ProjectExperience>()
+            .CustomInstantiator(_ => new ProjectExperience())
+            .RuleFor(o => o.Name, f => f.Lorem.Word())
+            .RuleFor(o => o.Url, f => f.Internet.Url())
+            .RuleFor(o => o.StartDate, f => f.Date.Past())
+            .RuleFor(o => o.EndDate, f => f.Date.Past())
+            .RuleFor(o => o.Description, f => f.Lorem.Paragraph())
+            .RuleFor(o => o.Highlights, f => new List<string> { f.Lorem.Sentence() })
+            .GenerateBetween(1, 2);
 }
---
+++ blazing-resume.pdf/Basic/Document_Plain.cs
@@ -0,0 +1,32 @@
+﻿using blazing_resume.models;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace blazing_resume.pdf.Basic;
+
+public sealed class Document_Plain : IDocument
+{
+    private readonly ResumeModel _model;
+
+    public Document_Plain(ResumeModel model)
+    {
+        _model = model;
+    }
+
+    public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+    public DocumentSettings GetSettings() => DocumentSettings.Default;
+
+    public void Compose(IDocumentContainer container)
+    {
+        container
+            .Page(page =>
+            {
+                page.Margin(50);
+
+                page.Header().Height(100).Background(Colors.Grey.Lighten1);
+                page.Content().Background(Colors.Grey.Lighten3);
+                page.Footer().Height(50).Background(Colors.Grey.Lighten1);
+            });
+    }
+}
